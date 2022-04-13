6594b33341fe30d387357338789eac20897f717d
final <63569156+FinalLabs@users.noreply.github.com>
2022-04-13 10:35:02 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,7 +4,12 @@ int main()
 {
 	interpreter interpreter({
 		"var hi_world = \"hi world\"",
-		"out hi_world"
+		"out hi_world",
+
+		"var test = 30",
+		"mul test by 2",
+		"div test by 3",
+		"out test"
 		});
 	
 	interpreter.load();
